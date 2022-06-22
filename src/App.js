00dd1247e902af91cfc9122f8aab150e8b1fb0b8
emanmbaba <emmanuel.mbaba@hightechstudios.io>
2022-06-22 16:46:03 -0500

    Eleventh Section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-const stories = [
+const initialStories = [
     {
         title: 'React',
         url: 'https://reactjs.org/',
@@ -37,6 +37,16 @@ const App = () => {
         'React'
     ); 
 
+    const [stories, setStories] = React.useState(initialStories);
+
+    const handleRemoveStory = (item) => {
+        const newStories = stories.filter(
+            (story) => item.objectID !== story.objectID
+        );
+
+        setStories(newStories);
+    }
+
     const handleSearch = (event) => {
       setSearchTerm(event.target.value);
     }
@@ -58,7 +68,7 @@ const App = () => {
           </InputWithLabel>
 
           <hr />
-          <List list={searchedStories} />
+          <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
       </div>
   );
 }
@@ -95,25 +105,38 @@ const InputWithLabel = ({
     )
 }
 
-const List = ( {list} ) => {
+const List = ( {list, onRemoveItem } ) => {
     return (
           <ul>
-              {list.map( ({ objectID, ...item }) =>  {   
-                  return <Item key={objectID} {...item} />
+              {list.map( (item) =>  {   
+                  return <Item 
+                      key={item.objectID}
+                      item={item}
+                      onRemoveItem={onRemoveItem}
+                      />
               })}
           </ul>
     );
 }
 
-const Item = ( { title, url, author, num_comments, points } ) => {
+const Item = ( { item, onRemoveItem } ) => {
+      const handleRemoveItem = () => {
+          onRemoveItem(item);
+      };
+
       return(
           <li>
               <span>
-                  <a href={url}>{title}</a>
+                  <a href={item.url}>{item.title}</a>
               </span>
-              <span>{author}</span>
-              <span>{num_comments}</span>
-              <span>{points}</span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                  <button type="button" onClick={handleRemoveItem}>
+                      Dismiss
+                  </button>
+              </span>
           </li>
       )
 }
